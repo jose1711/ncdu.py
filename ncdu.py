cc8690de068a0f3770f245bd96036dea94c34893
Jose Riha <jose1711@gmail.com>
2020-12-17 00:35:34 +0100

    Initial commit of ncdu.py

---
+++ ncdu.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+'''
+
+ncdu.py scans a given directory and exports json that can be fed to ncdu
+(https://dev.yorhel.nl/ncdu)
+
+It also allows to use time and name-based filters:
+  --newer-than D    = only include files modified less than D days ago
+  --older-than D    = only include files modified at least D days ago
+  --exclude PATTERN = works the same way as in ncdu, may be specified
+                      multiple times
+
+Scanning is significantly slower than with ncdu but you may still find
+this useful on machines/platform when compilation may be difficult/impossible
+while python is available.
+
+Example usage:
+
+# browse files modified in the last 5 days
+./ncdu.py --newer-than 5 /home | ncdu -f -
+'''
+
+from logging import debug
+from os import walk, stat, chdir, getcwd
+from os.path import basename as bn
+from pprint import pprint
+from time import time
+import argparse
+import fnmatch
+import json
+import logging
+import os.path
+import stat as _stat
+import sys
+
+def check_dir(entry):
+    path = entry.get('path')
+    notreg = entry.get('notreg')
+    debug(f'path: {path}, notreg: {notreg}')
+    # is this directory a symbolic link?
+    if notreg:
+        entry['asize'] = 0
+        entry['dsize'] = 0
+        return entry
+    ret = []
+
+    for rootname, dirnames, filenames in walk(path):
+        dirnames = list(dirnames)
+        debug(f'dirnames in {path}: {dirnames}')
+        ret = [entry]
+        fentries = []
+
+        for dirname in dirnames:
+            debug(f'processing dir {dirname}..')
+            metadata = stat(os.path.join(path, dirname), follow_symlinks=False)
+            asize = metadata.st_size
+            dsize = metadata.st_blocks * 512
+            dev = metadata.st_dev
+            inode = metadata.st_ino
+            notreg = _stat.S_ISLNK(stat(os.path.join(path, dirname), follow_symlinks=False).st_mode)
+            dentry = {"path": os.path.join(path, dirname),
+                      "name": bn(dirname),
+                      "asize": asize,
+                      "dsize": dsize,
+                      "dev": dev,
+                      "notreg": notreg,
+                      "ino": inode}
+            ret.extend([check_dir(dentry)])
+
+        for filename in filenames:
+            try:
+                metadata = stat(os.path.join(path, filename), follow_symlinks=False)
+            except:
+                # skip files that cannot be read
+                continue
+
+            excluded = False
+            asize = metadata.st_size
+            dsize = metadata.st_blocks * 512
+            dev = metadata.st_dev
+            mode = metadata.st_mode
+            inode = metadata.st_ino
+            mtime = int(metadata.st_mtime)
+            notreg = not _stat.S_ISREG(mode)
+
+            if args.exclude:
+                for pattern in args.exclude:
+                    if fnmatch.filter([filename], pattern):
+                        debug(f"Resetting {filename}'s size to 0 because of exclude pattern match")
+                        asize = 0
+                        dsize = 0
+                        excluded = True
+                        break
+
+            if args.older_than and (now - mtime) <= int(args.older_than) * 3600 * 24:
+                debug(f'Skipping {filename} due to older_than condition')
+                continue
+
+            if args.newer_than and (now - mtime) >= int(args.newer_than) * 3600 * 24:
+                debug(f'Skipping {filename} due to newer_than condition')
+                continue
+
+            fentry = {"path": os.path.join(path, filename),
+                      "name": filename,
+                      "asize": asize,
+                      "dsize": dsize,
+                      "notreg": notreg,
+                      "mode": mode,
+                      "dev": dev,
+                      "mtime": mtime,
+                      "ino": inode}
+            if metadata.st_nlink > 1:
+                fentry['hlnkc'] = True
+            if excluded:
+                fentry['excluded'] = 'pattern'
+            debug(f'filename: {filename}')
+            fentries.append(fentry)
+
+        debug(f'fentries: {fentries}')
+        if fentries: ret.extend(fentries)
+
+        debug(f'returning: {ret}')
+        return ret
+
+    # looks like the path is unreadable (permission issue?)
+    entry['read_error'] = True
+    return entry
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', nargs=1, help='Path to search in')
+parser.add_argument('-d', dest='debug', action='store_true', help='Debug mode')
+parser.add_argument('-o', dest='outfile',  help='write output to file (default: STDOUT)')
+parser.add_argument('--older-than', dest='older_than',  metavar='D', help='only include files older than D days', default=0)
+parser.add_argument('--newer-than', dest='newer_than',  metavar='D', help='only include files newer than D days', default=99999999)
+parser.add_argument('--exclude', dest='exclude',  metavar='PATTERN', action='append', help='exclude files that match PATTERN (may be specified multiple times)')
+
+args = parser.parse_args()
+path = args.path[0]
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+root = {'name': path, 'path': path,
+        'asize': 4096,
+        'dsize': 4096,
+        'dev': 1,
+        'ino': 1}
+
+now = int(time())
+data = check_dir(root)
+output = [1, 1, {"progname": "ncdu",
+                 "progver": "1.15.1",
+                 "timestamp": now}, data]
+
+if args.outfile:
+    outfile = args.outfile
+    if outfile == '-': 
+        print(json.dumps(output))
+    else:
+        with open(outfile, 'w') as f:
+            f.write(json.dumps(output))
+else:
+    print(json.dumps(output))
